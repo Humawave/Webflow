63973296dff3b13aa654fbd9253f6ba66e618af1
Humawave <160267997+Humawave@users.noreply.github.com>
2024-02-29 19:56:10 -0500

    Delete staggerAnimation.js

--- staggerAnimation.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const cmsItems = document.querySelectorAll('.cms_list .cms_item');
-
-    cmsItems.forEach((item, index) => {
-        item.style.animationDelay = `${index * 100}ms`;
-    });
-});
