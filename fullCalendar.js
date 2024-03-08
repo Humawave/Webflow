771145b2d3665ddd8dbf997f8cf5bf5b0626dcf0
Humawave <160267997+Humawave@users.noreply.github.com>
2024-03-07 19:41:19 -0500

    Update fullCalendar.js

--- fullCalendar.js
+++ fullCalendar.js
@@ -1,9 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
-        plugins: [ 'dayGrid' ], // If FullCalendar v5 or newer
-        initialView: 'dayGridMonth',
-        firstDay: 1, // Set Monday as the first day of the week
+      initialView: 'dayGridMonth'
     });
     calendar.render();
-});
+  });
