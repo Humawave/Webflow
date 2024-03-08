6dca600832fc459ad3e042d7dcb6b9ac752b28ac
Humawave <160267997+Humawave@users.noreply.github.com>
2024-03-07 19:26:08 -0500

    Update fullCalendar.js

--- fullCalendar.js
+++ fullCalendar.js
@@ -1,10 +1,7 @@
-// calendarSetup.js
 document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        plugins: ['dayGrid'],
-        initialView: 'dayGridMonth',
-        // Customize your calendar options here
-    });
-    calendar.render();
-});
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth'
+        });
+        calendar.render();
+      });
