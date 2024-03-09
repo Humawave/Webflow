3fe19d65bfe32ea6a96d26aefeda95f523d8a281
Humawave <160267997+Humawave@users.noreply.github.com>
2024-03-08 19:19:23 -0500

    Delete fullCalendar.js

--- fullCalendar.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // Instead of attaching to an input, attach the datepicker directly to a div
-  new AirDatepicker('#calendar', {
-    inline: true, // This option tells Air Datepicker to always show the calendar
-    // You can add other options here as needed
-  });
-});
