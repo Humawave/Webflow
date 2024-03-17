ce940818e1eff31ca0b8930fc4c4fca80160f2b1
Humawave <160267997+Humawave@users.noreply.github.com>
2024-03-17 01:09:14 -0400

    Delete hiddenInput.js

--- hiddenInput.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // Get all radio buttons for Step 1
-  document.querySelectorAll('input[name="step-1-choice"]').forEach(function(radioButton) {
-    radioButton.addEventListener('change', function() {
-      // Update the hidden input field with the value of the selected radio button
-      document.getElementById('step1Selection').value = this.value;
-    });
-  });
-});
