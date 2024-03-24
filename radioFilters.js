146597525eaf01519a488c509e6b96b99284b398
Humawave <160267997+Humawave@users.noreply.github.com>
2024-03-24 17:31:23 -0400

    Update radioFilters.js

--- radioFilters.js
+++ radioFilters.js
@@ -1,46 +1,42 @@
 document.addEventListener('DOMContentLoaded', function() {
     let isFirstLoad = true; // Flag to check if it's the initial page load
 
+    // Function for filtering items by category and potentially scrolling
     function filterItemsByCategory(selectedCategory, isUserInitiated = false) {
         const cmsItems = document.querySelectorAll('.cms_item');
-        let anyVisible = false; // Track if any items are visible
+        let anyVisible = false;
 
         cmsItems.forEach(function(item) {
             let belongsToCategory = false;
-
-            // Check each category div within the item for a match
             for (let i = 1; i <= 5; i++) {
                 const categoryDiv = item.querySelector(`.cms_categories .cms_title-category-${i}`);
-                
                 if (categoryDiv && categoryDiv.id === selectedCategory) {
                     belongsToCategory = true;
-                    break; // Stop checking further if a match is found
+                    break;
                 }
             }
 
             item.style.display = belongsToCategory || selectedCategory === 'all-stores' ? '' : 'none';
             if (item.style.display !== 'none') {
-                anyVisible = true; // Mark that we have at least one item visible
+                anyVisible = true;
             }
         });
 
-        // After checking all items, display the 'cms_list-empty' div if no items are visible
         const emptyListDiv = document.querySelector('.cms_list-empty');
         if (emptyListDiv) {
             emptyListDiv.style.display = anyVisible ? 'none' : 'block';
         }
 
-        // Update the results count
         if (window.updateResultsCount) {
             window.updateResultsCount();
         }
 
-        // Smooth scroll to the anchor after filtering, but only if it's a user-initiated action
         if (isUserInitiated) {
             smoothScrollToAnchor();
         }
     }
 
+    // Function to smoothly scroll to an anchor
     function smoothScrollToAnchor() {
         const anchorElement = document.getElementById('anchor');
         if (anchorElement) {
@@ -68,6 +64,26 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    // After the initial setup, mark that any further actions are user-initiated
     isFirstLoad = false;
+
+    // Check and handle horizontal scrolling
+    var scrollButton = document.getElementById('scroll-button');
+    var scrollContainer = document.getElementById('scrollable-container');
+
+    function checkScroll() {
+        if (scrollContainer.scrollWidth - Math.abs(scrollContainer.scrollLeft) - scrollContainer.clientWidth < 1) {
+            scrollButton.classList.add('hidden');
+        } else {
+            scrollButton.classList.remove('hidden');
+        }
+    }
+
+    scrollButton.addEventListener('click', function() {
+        var scrollValue = scrollContainer.offsetWidth;
+        scrollContainer.scrollBy({ left: scrollValue, behavior: 'smooth' });
+        setTimeout(checkScroll, 500); // Adjust time as necessary
+    });
+
+    scrollContainer.addEventListener('scroll', checkScroll);
+    checkScroll(); // Initial check
 });
