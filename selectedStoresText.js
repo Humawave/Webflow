71096b81964cb8c74c21ac097763bee418bf5a55
Humawave <160267997+Humawave@users.noreply.github.com>
2024-04-01 00:08:33 -0400

    Update selectedStoresText.js

--- selectedStoresText.js
+++ selectedStoresText.js
@@ -36,6 +36,7 @@ const storeNames = {
     'H7':  'HomeSense',
     'H9':  'Hot Topic',
     'H10':  'Hudson\'s Bay',
+    'I1':  'IKEA',
     'L14': 'Loblaws',
     'L15': 'Lululemon',
     'S4':  'Sephora',
