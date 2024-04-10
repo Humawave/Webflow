1b737173926a874f2c1d3791751bd0c44421f34f
Humawave <160267997+Humawave@users.noreply.github.com>
2024-04-10 18:06:58 -0400

    Update rudderStack.js

--- rudderStack.js
+++ rudderStack.js
@@ -2,7 +2,7 @@
   // Generates a UUID (Universally Unique Identifier)
   function generateUUID() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = (Math.random() * 16) | 0, v = c === 'x' ? r : (r & 0x3) | 0x8;
+      var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
       return v.toString(16);
     });
   }
@@ -11,16 +11,36 @@
   var userSessionId = generateUUID();
 
   // Initialize RudderStack
-  !function(){var analytics=window.rudderanalytics=window.rudderanalytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Rudder snippet included twice.");else{analytics.invoked=!0;analytics.methods=["load","page","track","identify","alias","group","reset","getAnonymousId","setAnonymousId"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.rudderlabs.com/v1/rudder-analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="1.0";
-    analytics.load("2ekYwbGNs81riCmMq0mDILYaZdc", "https://humawaveawapkr.dataplane.rudderstack.com");
+  !function(){
+    var analytics = window.rudderanalytics = window.rudderanalytics || [];
+    if (!analytics.initialize) {
+      if (analytics.invoked) {
+        window.console && console.error && console.error("Rudder snippet included twice.");
+      } else {
+        analytics.invoked = true;
+        analytics.methods = ["load","page","track","identify","alias","group","reset","getAnonymousId","setAnonymousId"];
+        analytics.factory = function(method){
+          return function(){
+            var args = Array.prototype.slice.call(arguments);
+            args.unshift(method);
+            analytics.push(args);
+            return analytics;
+          };
+        };
+        for (var i = 0; i < analytics.methods.length; i++) {
+          var key = analytics.methods[i];
+          analytics[key] = analytics.factory(key);
+        }
+        analytics.load("2ekYwbGNs81riCmMq0mDILYaZdc", "https://humawaveawapkr.dataplane.rudderstack.com");
+      }
+    }
+
     // Identify the user with the generated UUID
-    analytics.identify(userSessionId, {
-      sessionId: userSessionId
-    });
+    analytics.identify(userSessionId, { sessionId: userSessionId });
+
     // Automatically track page views
     analytics.page();
-  }})();
+  }();
 
-  // Additional tracking events can be added here, for example:
-  // rudderanalytics.track('Event Name', { property1: 'value1', property2: 'value2' });
+  // Additional tracking events can be added here
 })();
