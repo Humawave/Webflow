665fdd9acd7688cdf478fc415ebd9a91a5cdfddb
Humawave <160267997+Humawave@users.noreply.github.com>
2024-06-16 10:27:15 -0400

    Update rudderStack.js

--- rudderStack.js
+++ rudderStack.js
@@ -1,36 +1,41 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  // Generate a UUID
-  function generateUUID() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random() * 16 | 0,
-          v = c == 'x' ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-    });
-  }
-
-  // Generate a unique ID for each visitor if it doesn't already exist
-  function getUniqueVisitorId() {
-    let visitorId = localStorage.getItem('uniqueVisitorId');
-    if (!visitorId) {
-      visitorId = generateUUID();
-      localStorage.setItem('uniqueVisitorId', visitorId);
+    // Generate a UUID
+    function generateUUID() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random() * 16 | 0,
+                v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
     }
-    return visitorId;
-  }
 
-  // Get or create a unique visitor ID
-  const visitorId = getUniqueVisitorId();
+    // Generate a unique ID for each visitor if it doesn't already exist
+    function getUniqueVisitorId() {
+        let visitorId = localStorage.getItem('uniqueVisitorId');
+        if (!visitorId) {
+            visitorId = generateUUID();
+            localStorage.setItem('uniqueVisitorId', visitorId);
+        }
+        return visitorId;
+    }
 
-  // Identify the visitor with RudderStack
-  rudderanalytics.identify(visitorId);
+    // Get or create a unique visitor ID
+    const visitorId = getUniqueVisitorId();
 
-  // Track the page view
-  rudderanalytics.page();
+    // Identify the visitor with RudderStack
+    rudderanalytics.identify(visitorId);
 
-  // Function to track store selection
-  window.trackStoreSelection = function(storeName) {
-    rudderanalytics.track('store_selected', {
-      store_name: storeName
-    });
-  };
+    // Track the page view
+    rudderanalytics.page();
+
+    // Function to track store selection
+    window.trackStoreSelection = function(storeName) {
+        rudderanalytics.track('store_selected', {
+            store_name: storeName
+        }, {
+            externalId: [{
+                type: "ga4ClientId",
+                id: "client_id"
+            }]
+        });
+    };
 });
