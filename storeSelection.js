f05211e804c22a3780a95502943377b29229cb77
Humawave <160267997+Humawave@users.noreply.github.com>
2024-07-03 18:03:02 -0400

    Update storeSelection.js

--- storeSelection.js
+++ storeSelection.js
@@ -89,6 +89,9 @@ window.addEventListener('load', () => {
                 L14: 'Loblaws',
                 L15: 'Lululemon',
                 L16: 'Lush',
+                M1: 'MAC',
+                M3: 'Mango',
+                M6: 'Michael Kors',
             };
 
             // Toggle selection
