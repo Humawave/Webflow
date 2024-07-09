f0b08968f6f161ec5e8981bd11aa7d5a6067079b
Humawave <160267997+Humawave@users.noreply.github.com>
2024-07-09 19:53:16 -0400

    Update firebaseSetup.js

--- firebaseSetup.js
+++ firebaseSetup.js
@@ -1,8 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
-import { getAuth, setPersistence, browserLocalPersistence, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js';
+import { getAuth, setPersistence, browserLocalPersistence, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js';
-import { getTimeGreeting, getTimeEmoji, convertTo24HourFormat, calculateTimeDifference } from './utilityFunctions.js';
-import { fetchUserData, fetchBookings } from './dataFetchers.js';
 
 async function fetchFirebaseConfig() {
   try {
@@ -17,7 +15,7 @@ async function fetchFirebaseConfig() {
   }
 }
 
-async function initializeFirebase() {
+export async function initializeFirebase() {
   const firebaseConfig = await fetchFirebaseConfig();
 
   const app = initializeApp(firebaseConfig);
@@ -26,138 +24,40 @@ async function initializeFirebase() {
 
   await setPersistence(auth, browserLocalPersistence);
 
-  return { app, auth, db };
+  return { app, auth, db, onAuthStateChanged };
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
-  const loader = document.getElementById('account-loading');
-
-  function showLoader() {
-    if (loader) {
-      loader.style.display = 'flex';
-    }
-  }
-
-  function hideLoader() {
-    if (loader) {
-      loader.classList.add('hide');
-      setTimeout(() => {
-        loader.style.display = 'none';
-        loader.classList.remove('hide');
-      }, 100);
-    }
-  }
-
   try {
-    showLoader();
-    const { auth, db } = await initializeFirebase();
+    const { auth, onAuthStateChanged } = await initializeFirebase();
 
-    onAuthStateChanged(auth, async (user) => {
+    const myAccountButton = document.getElementById('my-account');
+
+    onAuthStateChanged(auth, (user) => {
       if (user) {
-        const userData = await fetchUserData(db, user.email);
-        const bookings = await fetchBookings(db, user.email);
-
-        if (userData) {
-          const { firstName, lastName, referralCode } = userData;
-          const greeting = getTimeGreeting();
-          const emoji = getTimeEmoji();
-
-          const accountWelcomeElement = document.getElementById('account-welcome');
-          if (accountWelcomeElement) {
-            accountWelcomeElement.textContent = `${greeting}, ${firstName} ${emoji}`;
-          }
-
-          const referralCodeInputElement = document.getElementById('account-referral-code');
-          if (referralCodeInputElement) {
-            referralCodeInputElement.value = referralCode;
-            referralCodeInputElement.readOnly = true;
-          }
-
-          const shoppingComponent = document.getElementById('shopping-component');
-          if (shoppingComponent) {
-            shoppingComponent.classList.add('hidden');
-
-            bookings.forEach(booking => {
-              const bookingElement = shoppingComponent.cloneNode(true);
-              bookingElement.classList.remove('hidden');
-              const bookingDate = new Date(`${booking.day}T00:00:00`);
-              const options = { weekday: 'long', month: 'long', day: 'numeric' };
-              const bookingDateString = bookingDate.toLocaleDateString(undefined, options);
-
-              const shoppingDateElement = bookingElement.querySelector('#shopping-date');
-              if (shoppingDateElement) {
-                shoppingDateElement.textContent = bookingDateString;
-              }
-
-              const shoppingStoreElement = bookingElement.querySelector('#shopping-stores');
-              if (shoppingStoreElement) {
-                shoppingStoreElement.textContent = booking.stores;
-              }
-
-              const shoppingTimeElement = bookingElement.querySelector('#shopping-time');
-              if (shoppingTimeElement) {
-                shoppingTimeElement.textContent = booking.time;
-              }
-
-              const accountFreeInitialElement = bookingElement.querySelector('#account-free-initial');
-              const accountStandardSessionElement = bookingElement.querySelector('#account-standard-session');
-              if (booking.referral && accountFreeInitialElement && accountStandardSessionElement) {
-                accountFreeInitialElement.style.display = 'block';
-                accountStandardSessionElement.style.display = 'none';
-              } else if (accountFreeInitialElement && accountStandardSessionElement) {
-                accountFreeInitialElement.style.display = 'none';
-                accountStandardSessionElement.style.display = 'block';
-              }
-
-              const joinSessionButton = bookingElement.querySelector('#join-session');
-              if (joinSessionButton) {
-                const roomUrlWithName = `${booking.room}?name=${firstName}%20${lastName}`;
-                joinSessionButton.setAttribute('data-room-url', roomUrlWithName);
-                joinSessionButton.addEventListener('click', () => {
-                  localStorage.setItem('roomURL', roomUrlWithName);
-                  window.location.href = 'https://www.humawave.com/account/session';
-                });
-
-                const joinInTextElement = bookingElement.querySelector('#join-in');
-                if (joinInTextElement) {
-                  const formattedTime = convertTo24HourFormat(booking.time);
-                  const sessionDateString = `${booking.day}T${formattedTime}:00`;
-                  const sessionTime = new Date(sessionDateString.replace("AM", "").replace("PM", ""));
-                  const updateJoinInText = () => {
-                    const timeDiff = calculateTimeDifference(sessionTime);
-                    joinInTextElement.textContent = `Join in ${timeDiff.hours} hours, ${timeDiff.minutes} minutes`;
-                  };
-
-                  updateJoinInText();
-                  setInterval(updateJoinInText, 60000);
-                }
-              }
-
-              shoppingComponent.parentNode.appendChild(bookingElement);
-            });
-          }
+        // User is signed in, set the account button to redirect to the account page
+        console.log('User is signed in:', user);
+        if (myAccountButton) {
+          myAccountButton.addEventListener('click', () => {
+            window.location.href = 'https://www.humawave.com/account';
+          });
         }
       } else {
-        const accountWelcomeElement = document.getElementById('account-welcome');
-        if (accountWelcomeElement) {
-          accountWelcomeElement.textContent = "Welcome!";
+        // No user is signed in, set the account button to redirect to the welcome page
+        console.log('No user is signed in.');
+        if (myAccountButton) {
+          myAccountButton.addEventListener('click', () => {
+            window.location.href = 'https://www.humawave.com/welcome';
+          });
+        }
+
+        // Redirect to homepage if trying to access account pages
+        if (window.location.pathname.startsWith('/account')) {
+          window.location.href = 'https://www.humawave.com/';
         }
       }
     });
-
-    const signOutButton = document.getElementById('account-sign-out');
-    if (signOutButton) {
-      signOutButton.addEventListener('click', () => {
-        signOut(auth).then(() => {
-          window.location.href = 'https://www.humawave.com/';
-        }).catch((error) => {
-          console.error('Error signing out:', error);
-        });
-      });
-    }
   } catch (error) {
     console.error('Error initializing Firebase:', error);
-  } finally {
-    hideLoader();
   }
 });
